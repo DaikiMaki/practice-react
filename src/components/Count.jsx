3d5cacce4b538aad2ef950bd294987f74e66ab5f
makidaiki9180@gmail.com <makidaiki9180@gmail.com>
2022-11-19 10:01:23 +0800

    ライフサイクルと副作用（useEffect）

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,7 +1,12 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Count = () => {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffectが実行されました");
+  }, [count2])
 
   const countUp = () => {
     setCount(count + 1);
@@ -16,6 +21,9 @@ const Count = () => {
       <p>現在のカウント：{count}</p>
       <button onClick={() => countUp()}>+</button>
       <button onClick={() => countDown()}>-</button>
+      <p>現在のカウント（２）：{count2}</p>
+      <button onClick={() => setCount2(count2+1)}>+</button>
+      <button onClick={() => setCount2(count2-1)}>-</button>
     </>
   )
 }
